545268dae7d3a51a79bb4735b0177e91b002d21c
Carter <122408763+cartrleitch@users.noreply.github.com>
2025-12-11 22:06:57 -0600

    Add files via upload

---
+++ netflixautoplay.py
@@ -0,0 +1,42 @@
+import pyautogui
+import time
+
+p = pyautogui
+
+p.FAILSAFE = True
+
+p.pause = 1
+
+screen_size = p.size()
+screen_center_x = screen_size[0]
+screen_center_y = screen_size[1]
+
+print(f"Screen size: {screen_size}")
+print(int(screen_center_x/2))
+print("Automatically skipping intro and selecting next episode on Netflix!")
+
+def move_center():
+    p.moveTo(int(screen_center_x/2), int(screen_center_y/2), 0)
+move_center()
+while True:
+    try:
+        next_episode_button_location = p.locateCenterOnScreen(r'next_episode.png', confidence=0.5)
+        print("Locating next episode: " + str(next_episode_button_location))
+        p.leftClick(next_episode_button_location)
+        move_center()
+
+    except p.ImageNotFoundException:
+        #print("Next episode button not on screen.")
+        pass
+
+    try:
+        skip_intro_button_location = p.locateCenterOnScreen(r'skip_intro.png', confidence=0.8)
+        print("Locating skip intro: "+ str(skip_intro_button_location))
+        p.leftClick(skip_intro_button_location)
+        move_center()
+
+    except p.ImageNotFoundException:
+        #print("Skip intro button not on screen.")
+        pass
+    
+    time.sleep(0.5)
